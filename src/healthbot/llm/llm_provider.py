628358b517a3b4c7c6e2a4f4b9a66533511c1c6f
Mamoutou <mamoutoufofana@mac.home>
2026-03-07 13:56:48 +0100

    feat(llm): add implementation in llm_provider

---
+++ src/healthbot/llm/llm_provider.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+from src.healthbot.config.settings import settings
+
+class LLMProvider:
+    """
+    Factory responsible for creating LLM instances.
+    """
+
+    def __init__(self):
+        self._llm = None
+
+    def get_model(self) -> ChatOpenAI:
+        """
+        Return configured ChatOpenAI instance.
+
+        Returns
+        -------
+        ChatOpenAI
+        """
+        if self._llm is None:
+            self._llm = ChatOpenAI(
+                model=settings.model_name,
+                temperature=0,
+                api_key=settings.openai_api_key
+            )
+
+        return self._llm
