34b18db3dfe5870067c11e58057b17e282d5f134
Mamoutou <mamoutoufofana@mac.home>
2026-03-08 00:53:46 +0100

    feat(scripts): Add agent running implementation

---
+++ scripts/run_healthbot.py
@@ -0,0 +1,7 @@
+from src.healthbot.core.logging import configure_logging
+from src.healthbot.cli.main import human_in_the_loop
+configure_logging()
+
+
+if __name__ == "__main__":
+    human_in_the_loop()
