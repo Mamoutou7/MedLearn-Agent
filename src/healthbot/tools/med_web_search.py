8fd80f53ca809ee43e485d8c0a7427e79ebef8bf
Mamoutou <mamoutoufofana@mac.home>
2026-03-07 14:08:22 +0100

    feat(tools): add tavily web search implementation

---
+++ src/healthbot/tools/med_web_search.py
@@ -0,0 +1,27 @@
+"""
+External tool for retrieving health information from the web.
+"""
+
+from typing import Dict
+from langchain_core.tools import tool
+from tavily import TavilyClient
+from src.healthbot.config.settings import settings
+
+@tool
+def med_web_search(question: str) -> Dict:
+    """
+    Perform a web search for medical information.
+
+    Parameters
+    ----------
+    question : str
+        User question related to health.
+
+    Returns
+    -------
+    Dict
+        Tavily search results.
+    """
+    client = TavilyClient(api_key=settings.tavily_api_key)
+
+    return client.search(question)
